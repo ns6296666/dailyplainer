d6e3b16304565d153235fefc31143e1b8c30f1ec
Harry <harry@macbookpro>
2024-02-21 23:53:22 +0000

    created li element

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const date = dayjs(new Date());
 const day = date.format("dddd, MMMM D");
 
-const currentDayElement = document.getElementById("currentDay");
-currentDayElement.textContent = day;
+/// file icon
+const iconElement = document.getElementsByClassName("save-file");
+
+// logic for creating business hour
 const timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 const result = timeArray.map((t) => {
   const ampm = t % 12;
-  console.log("ampm", ampm);
-  return `${ampm === 0 ? 12 : ampm}${t >= 12 ? "pm" : "am"}`;
+
+  return {
+    time: `${ampm === 0 ? 12 : ampm}${t >= 12 ? "pm" : "am"}`,
+    inputText: iconElement,
+    fileIcon: "file.png",
+  };
 });
-console.log(result);
+
+const currentDayElement = document.getElementById("currentDay");
+currentDayElement.textContent = day;
+
+// business hour element
+const parentElement = document.getElementById("hours-list");
+
+let liElement = document.createElement("li");
+liElement = result.map((hour) => {
+  return (liElement.textContent = hour);
+});
+parentElement.append(liElement);
+console.log("ampm", liElement);
