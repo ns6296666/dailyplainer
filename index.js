fa6cefe3fe5530451901f76f8e3057f8f3eeb809
Harry <harry@macbookpro>
2024-02-21 23:24:40 +0000

    created business hours array

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const day = date.format("dddd, MMMM D");
 
 const currentDayElement = document.getElementById("currentDay");
 currentDayElement.textContent = day;
+const timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
+const result = timeArray.map((t) => {
+  const ampm = t % 12;
+  console.log("ampm", ampm);
+  return `${ampm === 0 ? 12 : ampm}${t >= 12 ? "pm" : "am"}`;
+});
+console.log(result);
