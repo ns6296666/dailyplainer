33a1df39cf2d2131a002b947ba4f4d0e8c1d18dd
Harry <harry@macbookpro>
2024-02-22 01:40:59 +0000

    css design complete

--- index.js
+++ index.js
@@ -1,19 +1,6 @@
 const date = dayjs(new Date());
 const day = date.format("dddd, MMMM D");
 
-// input element
-const inputText = document.createElement("input");
-inputText.type = "text";
-inputText.placeholder = "Enter your text";
-console.log(inputText);
-
-// file icon
-const iconElement = document.createElement("i");
-iconElement.classList = "fa-solid fa-file";
-iconElement.style.color = "blue";
-iconElement.style.fontSize = "24px";
-console.log("iconElement", iconElement);
-
 // logic for creating business hour
 const timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 const result = timeArray.map((t) => {
@@ -32,16 +19,31 @@ const parentElement = document.getElementById("container");
 
 let liElement = result.forEach((hour) => {
   const divElement = document.createElement("div");
-  divElement.classList.add("available-hours");
+  divElement.classList.add("custom-row");
+
+  // input element
+  const inputText = document.createElement("input");
+  inputText.type = "text";
+
+  console.log(inputText);
+
+  // file icon
+  const iconElement = document.createElement("i");
+  iconElement.classList = "fa-solid fa-file";
+  iconElement.style.color = "blue";
+  iconElement.style.fontSize = "24px";
+  console.log("iconElement", iconElement);
+
   divElement.innerHTML = `
  
   <p class="hour">${hour.time}</p>
-  <div class="event-section">
-    <p class="detail">${inputText.value}</p>
-  </div>
-  <div class="icons">${iconElement.outerHTML}</div>
+
+  <p class="detail"></p>
+ <div class="icons"></div>
+ 
 `;
+  divElement.querySelector(".detail").appendChild(inputText);
+  divElement.querySelector(".icons").appendChild(iconElement);
   parentElement.append(divElement);
 });
-// parentElement.append(liElement);
 console.log("ampm", parentElement);
