654f5ec3e4e5da3164d0bf48d88cdbb529a6699a
Harry <harry@macbookpro>
2024-02-22 00:44:38 +0000

    created structure of app

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const date = dayjs(new Date());
 const day = date.format("dddd, MMMM D");
 
-/// file icon
-const iconElement = document.getElementsByClassName("save-file");
+// input element
+const inputText = document.createElement("input");
+inputText.type = "text";
+inputText.placeholder = "Enter your text";
+console.log(inputText);
+
+// file icon
+const iconElement = document.createElement("i");
+iconElement.classList = "fa-solid fa-file";
+iconElement.style.color = "blue";
+iconElement.style.fontSize = "24px";
+console.log("iconElement", iconElement);
 
 // logic for creating business hour
 const timeArray = [9, 10, 11, 12, 13, 14, 15, 16, 17];
@@ -11,8 +21,6 @@ const result = timeArray.map((t) => {
 
   return {
     time: `${ampm === 0 ? 12 : ampm}${t >= 12 ? "pm" : "am"}`,
-    inputText: iconElement,
-    fileIcon: "file.png",
   };
 });
 
@@ -20,11 +28,20 @@ const currentDayElement = document.getElementById("currentDay");
 currentDayElement.textContent = day;
 
 // business hour element
-const parentElement = document.getElementById("hours-list");
+const parentElement = document.getElementById("container");
 
-let liElement = document.createElement("li");
-liElement = result.map((hour) => {
-  return (liElement.textContent = hour);
+let liElement = result.forEach((hour) => {
+  const divElement = document.createElement("div");
+  divElement.classList.add("available-hours");
+  divElement.innerHTML = `
+ 
+  <p class="hour">${hour.time}</p>
+  <div class="event-section">
+    <p class="detail">${inputText.value}</p>
+  </div>
+  <div class="icons">${iconElement.outerHTML}</div>
+`;
+  parentElement.append(divElement);
 });
-parentElement.append(liElement);
-console.log("ampm", liElement);
+// parentElement.append(liElement);
+console.log("ampm", parentElement);
